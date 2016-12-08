1fd10ff4ed8853a68252072311e23979ba66b36e
Ranx0r0x <register@bradleejohnson.com>
2016-12-07 19:23:39 -0600

    Change the license to reflect Apache license

--- developmentkit/src/test/java/org/enjekt/panda/developmentdatstore/WhiteVaultDevelopmentDataStoreTest.java
+++ developmentkit/src/test/java/org/enjekt/panda/developmentdatstore/WhiteVaultDevelopmentDataStoreTest.java
@@ -1,3 +1,18 @@
+/*  Copyright 2016 Bradlee Johnson 
+ *  This file is part of Panda Vault.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**/
 package org.enjekt.panda.developmentdatstore;
 
 import static org.junit.Assert.assertEquals;
@@ -16,13 +31,23 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class WhiteVaultDevelopmentDataStoreTest.
+ */
 @RunWith(CamelCdiRunner.class)
 public class WhiteVaultDevelopmentDataStoreTest {
 
+	/** The datastore. */
 	@Inject
 	private WhiteVaultDatastore datastore;
 
 
+	/**
+	 * Gets the same token for pan.
+	 *
+	 * @return the same token for pan
+	 */
 	@Test
 	public void getSameTokenForPan()
 	{
@@ -37,6 +62,10 @@ public class WhiteVaultDevelopmentDataStoreTest {
 		assertNotNull(token);
 		assertEquals(tokenNew.getToken(),token.getToken());
 	}
+	
+	/**
+	 * Validate same pan for token.
+	 */
 	@Test
 	public void validateSamePanForToken()
 	{
@@ -49,6 +78,10 @@ public class WhiteVaultDevelopmentDataStoreTest {
 		assertNotNull(storedPan);
 		assertEquals(storedPan,storedPan);
 	}
+	
+	/**
+	 * Test pre loaded data.
+	 */
 	@Test
 	@Ignore
 	public void testPreLoadedData()
@@ -65,6 +98,8 @@ public class WhiteVaultDevelopmentDataStoreTest {
 		}
 		
 	}
+	
+	/** The token list. */
 	private static List<Token> tokenList = Arrays.asList(new Token[]{
 			new Token().setToken("372842011638689"),
 			new Token().setToken("447828108404444"),
